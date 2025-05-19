2a27c6cce4f49341dfea58183114f979fcc89288
vaishnavPatange <155091756+vaishnavPatange@users.noreply.github.com>
2025-05-19 23:52:53 +0530

    Update page.tsx

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -5,7 +5,7 @@ type userProfile = {
 }
 
 const UserProfile = async ({params}: userProfile) => {
-      const {id} = await params;
+      const {id} = params;
   return (
     <div className="flex justify-center min-h-screen items-center">
       <h1 className="text-4xl">user id: {id}</h1>
