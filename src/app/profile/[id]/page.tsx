1b6bd6491a75033267518c30c8f1535901fe0ece
vaishnavPatange <patangevaishnav5@gmail.com>
2025-05-20 10:27:53 +0530

    update page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,11 +1,11 @@
 type userProfile = {
   params: {
-    id: String
+    id: string
   }
 }
 
 const UserProfile = async ({params}: userProfile) => {
-      const {id} = await params;
+      const {id} = params;
   return (
     <div className="flex justify-center min-h-screen items-center">
       <h1 className="text-4xl">user id: {id}</h1>
